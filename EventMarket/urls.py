eac2add3706f6ab678fcf9b79c2c371cbbcde52d
Saryn7373 <winningson90@gmail.com>
2026-03-25 23:47:24 +0300

    api for venues with permissions and filters

--- EventMarket/urls.py
+++ EventMarket/urls.py
@@ -21,7 +21,9 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
+    path('api/venues/', include('venues.urls')),
     path('api/schema/',         SpectacularAPIView.as_view(),        name='schema'),
     path('api/docs/',           SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/redoc/',     SpectacularRedocView.as_view(url_name='schema'),   name='redoc'),
+    path('api-auth/', include('rest_framework.urls'))
 ]
---
+++ venues/filters.py
@@ -0,0 +1,23 @@
+import django_filters
+from .models import Venue
+
+
+class VenueFilter(django_filters.FilterSet):
+    city            = django_filters.CharFilter(lookup_expr='icontains')
+    status          = django_filters.ChoiceFilter(choices=Venue.STATUS_CHOICES)
+    is_verified     = django_filters.BooleanFilter()
+    capacity_min    = django_filters.NumberFilter(field_name='capacity_min', lookup_expr='gte')
+    capacity_max    = django_filters.NumberFilter(field_name='capacity_max', lookup_expr='lte')
+    price_hour_min  = django_filters.NumberFilter(field_name='price_per_hour', lookup_expr='gte')
+    price_hour_max  = django_filters.NumberFilter(field_name='price_per_hour', lookup_expr='lte')
+    price_day_min   = django_filters.NumberFilter(field_name='price_per_day',  lookup_expr='gte')
+    price_day_max   = django_filters.NumberFilter(field_name='price_per_day',  lookup_expr='lte')
+
+    class Meta:
+        model  = Venue
+        fields = [
+            'city', 'status', 'is_verified',
+            'capacity_min', 'capacity_max',
+            'price_hour_min', 'price_hour_max',
+            'price_day_min', 'price_day_max',
+        ]
